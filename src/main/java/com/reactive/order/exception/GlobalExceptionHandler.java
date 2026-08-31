f5e3d76210f5624b86ed8ef484e2a7850b94f498
Predeepkumar U S <usppcbe@gmail.com>
2026-08-31 17:46:19 +0530

    Exceptions handled

--- src/main/java/com/reactive/order/exception/GlobalExceptionHandler.java
+++ src/main/java/com/reactive/order/exception/GlobalExceptionHandler.java
@@ -1,4 +1,57 @@
 package com.reactive.order.exception;
 
+import com.reactive.order.model.entity.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import reactor.core.publisher.Mono;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    public Mono<ResponseEntity<ApiResponse<Void>>> handleOrderNotFound(
+            OrderNotFoundException ex) {
+
+        return Mono.just(
+                ResponseEntity.status(HttpStatus.NOT_FOUND)
+                        .body(
+                                new ApiResponse<>(
+                                        ex.getMessage(),
+                                        null
+                                )
+                        )
+        );
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public Mono<ResponseEntity<ApiResponse<Void>>> handleIllegalArgument(
+            IllegalArgumentException ex) {
+
+        return Mono.just(
+                ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body(
+                                new ApiResponse<>(
+                                        ex.getMessage(),
+                                        null
+                                )
+                        )
+        );
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public Mono<ResponseEntity<ApiResponse<Void>>> handleIllegalState(
+            IllegalStateException ex) {
+
+        return Mono.just(
+                ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body(
+                                new ApiResponse<>(
+                                        ex.getMessage(),
+                                        null
+                                )
+                        )
+        );
+    }
 }
